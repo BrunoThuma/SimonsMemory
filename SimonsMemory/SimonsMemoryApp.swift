f9d02a5a4d69c564ea0214a4d8f77a116432230e
brunothuma <bruno.thuma@hotmail.com>
2023-09-16 16:21:01 -0300

    Initial Commit

---
+++ SimonsMemory/SimonsMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonsMemoryApp.swift
+//  SimonsMemory
+//
+//  Created by Bruno Thuma on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SimonsMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
